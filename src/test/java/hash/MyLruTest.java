f1b19645f994172a9bbcdf70e94f8fa73ab3ffcf
hxd <xudong_hou@qq.com>
2019-11-06 20:24:48 +0800

    实现一个 LRU 缓存淘汰算法

---
+++ src/test/java/hash/MyLruTest.java
@@ -0,0 +1,30 @@
+package hash;
+
+import org.junit.Test;
+
+/**
+ * @author:hxd
+ * @date:2019/11/6
+ */
+public class MyLruTest {
+
+    @Test
+    public void lruTest() {
+        MyLru<Integer, String> lruCache = new MyLru();
+        for (int i = 1; i <= 10; i++) {
+            lruCache.put(i, "haha" + i);
+        }
+        System.out.println(lruCache);
+
+        lruCache.put(11, "later" + 11);
+        lruCache.put(11, "later" + 111);
+
+        lruCache.get(5);
+        lruCache.get(2);
+        lruCache.get(8);
+
+        lruCache.remove(3);
+
+        System.out.println(lruCache);
+    }
+}

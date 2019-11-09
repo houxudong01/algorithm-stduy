44d39381355dcd3b3184bb1f0242de7e5bb9371b
hxd <xudong_hou@qq.com>
2019-11-09 19:35:33 +0800

    二叉查找树插入、查找、删除，前中后序遍历和按层遍历相关实现

---
+++ src/test/java/binarytree/BinaryTreeTest.java
@@ -0,0 +1,55 @@
+package binarytree;
+
+import org.junit.Test;
+
+/**
+ * @author:hxd
+ * @date:2019/11/9
+ */
+public class BinaryTreeTest {
+
+    @Test
+    public void binaryTreeTest() {
+        BinarySearchTree binaryTree = new BinarySearchTree();
+        binaryTree.insert(33);
+        binaryTree.insert(17);
+        binaryTree.insert(50);
+        binaryTree.insert(13);
+        binaryTree.insert(18);
+        binaryTree.insert(34);
+        binaryTree.insert(58);
+        binaryTree.insert(16);
+        binaryTree.insert(25);
+        binaryTree.insert(51);
+        binaryTree.insert(66);
+        binaryTree.insert(19);
+        binaryTree.insert(27);
+        binaryTree.insert(55);
+        System.out.println(binaryTree);
+
+        System.out.println("先序遍历");
+        binaryTree.preOrder(binaryTree.getRoot());
+        System.out.println();
+
+        System.out.println("中序遍历");
+        binaryTree.midOrder(binaryTree.getRoot());
+        System.out.println();
+
+        System.out.println("后序遍历");
+        binaryTree.postOrder(binaryTree.getRoot());
+        System.out.println();
+
+        System.out.println("分层遍历");
+        binaryTree.levelOrder(binaryTree.getRoot());
+        System.out.println();
+
+        System.out.println();
+        System.out.println("查找：");
+        System.out.println(binaryTree.search(19));
+
+        System.out.println("删除：");
+        System.out.println(binaryTree.remove(50));
+        binaryTree.midOrder(binaryTree.getRoot());
+
+    }
+}
